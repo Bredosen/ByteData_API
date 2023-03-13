3a14a16587985bf3a3498bb3844030f0887367a7
Bredosen <Bredosen@dr.com>
2023-03-13 09:20:03 +0100

    Improved ByteData's type handling Added a new class for the ByteDataType, this allows for a more specific usage.

--- src/main/java/me/bredo/cmd/byteData/utilities/ByteData.java
+++ src/main/java/me/bredo/cmd/byteData/utilities/ByteData.java
@@ -2,75 +2,119 @@ package me.bredo.cmd.byteData.utilities;
 
 import me.bredo.cmd.byteData.handlers.TypeByteConverter;
 
-public final class ByteData<T> {
+/**
+ * ByteData class represents data in byte format, and provides methods to convert it to and from Java data types.
+ * It also stores the data type.
+ */
+public final class ByteData {
+    private final ByteDataType byteDataType;
+    private Object value;
 
-    private T type;
-
-    public ByteData(final T type) {
-        set(type);
+    /**
+     * Creates a new ByteData object with the given ByteDataType and value.
+     *
+     * @param byteDataType The ByteDataType of the value.
+     * @param value        The value to store.
+     */
+    public ByteData(final ByteDataType byteDataType, final Object value) {
+        this.byteDataType = byteDataType;
+        set(value);
     }
 
-    public ByteData() {
+    /**
+     * Creates a new ByteData object with a dynamically detected ByteDataType and value.
+     *
+     * @param value The value to store.
+     */
+    public ByteData(final Object value) {
+        if (value instanceof String) byteDataType = ByteDataType.String;
+        else if (value instanceof Integer) byteDataType = ByteDataType.Int;
+        else if (value instanceof Float) byteDataType = ByteDataType.Float;
+        else if (value instanceof Double) byteDataType = ByteDataType.Double;
+        else if (value instanceof Short) byteDataType = ByteDataType.Short;
+        else if (value instanceof Long) byteDataType = ByteDataType.Long;
+        else if (value instanceof Boolean) byteDataType = ByteDataType.Boolean;
+        else if (value instanceof String[]) byteDataType = ByteDataType.String_Array;
+        else {
+            byteDataType = null;
+            return;
+        }
+        set(value);
     }
 
-    public T get() {
-        return type;
+    /**
+     * Gets the stored value.
+     *
+     * @return The stored value.
+     */
+    public Object get() {
+        return value;
     }
 
-    public void set(final T type) {
-        this.type = type;
+    /**
+     * Sets the stored value.
+     *
+     * @param value The value to store.
+     */
+    public void set(final Object value) {
+        this.value = value;
     }
 
+    /**
+     * Gets the ByteDataType of the stored value.
+     *
+     * @return The ByteDataType of the stored value.
+     */
+    public ByteDataType getByteDataType() {
+        return byteDataType;
+    }
+
+    /**
+     * Converts the stored value from a byte array.
+     *
+     * @param bytes The byte array to convert from.
+     */
     public void convertFrom(final byte[] bytes) {
-        if (type instanceof String string) {
-            type = (T) TypeByteConverter.arrayToString(bytes);
-        } else if (type instanceof Integer integer) {
-            type = (T) TypeByteConverter.arrayToInt(bytes);
-        } else if (type instanceof Float floatValue) {
-            type = (T) TypeByteConverter.arrayToFloat(bytes);
-        } else if (type instanceof Double doubleValue) {
-            type = (T) TypeByteConverter.arrayToDouble(bytes);
-        } else if (type instanceof Short shortValue) {
-            type = (T) TypeByteConverter.arrayToShort(bytes);
-        } else if (type instanceof Long longValue) {
-            type = (T) TypeByteConverter.arrayToLong(bytes);
-        } else if (type instanceof Boolean booleanValue) {
-            type = (T) TypeByteConverter.byteToBoolean(bytes[0]);
-        } else if (type instanceof String[] stringArray) {
-            type = (T) TypeByteConverter.arrayToStringArray(bytes);
-        } else {
-            System.err.println("Type '" + type.getClass().getSimpleName() + "'could not be converted");
-        }
+        if (bytes == null) return;
+        if (byteDataType.equals(ByteDataType.String)) set(TypeByteConverter.arrayToString(bytes));
+        else if (byteDataType.equals(ByteDataType.Int)) set(TypeByteConverter.arrayToInt(bytes));
+        else if (byteDataType.equals(ByteDataType.Float)) set(TypeByteConverter.arrayToFloat(bytes));
+        else if (byteDataType.equals(ByteDataType.Double)) set(TypeByteConverter.arrayToDouble(bytes));
+        else if (byteDataType.equals(ByteDataType.Short)) set(TypeByteConverter.arrayToShort(bytes));
+        else if (byteDataType.equals(ByteDataType.Long)) set(TypeByteConverter.arrayToLong(bytes));
+        else if (byteDataType.equals(ByteDataType.Boolean)) set(TypeByteConverter.byteToBoolean(bytes[0]));
+        else if (byteDataType.equals(ByteDataType.String_Array)) set(TypeByteConverter.arrayToStringArray(bytes));
+        else System.err.println("Type '" + value.getClass().getSimpleName() + "'could not be converted");
     }
 
+    /**
+     * Converts the stored value to a byte array based on the ByteDataType.
+     *
+     * @return a byte array representation of the stored value
+     */
     public byte[] convert() {
+        if (get() == null) return new byte[0];
         byte[] bytes = null;
-        if (type instanceof String string) {
-            bytes = TypeByteConverter.stringToArray(string);
-        } else if (type instanceof Integer integer) {
-            bytes = TypeByteConverter.intToArray(integer);
-        } else if (type instanceof Float floatValue) {
-            bytes = TypeByteConverter.floatToArray(floatValue);
-        } else if (type instanceof Double doubleValue) {
-            bytes = TypeByteConverter.doubleToArray(doubleValue);
-        } else if (type instanceof Short shortValue) {
-            bytes = TypeByteConverter.shortToArray(shortValue);
-        } else if (type instanceof Long longValue) {
-            bytes = TypeByteConverter.longToArray(longValue);
-        } else if (type instanceof Boolean booleanValue) {
-            bytes = new byte[]{TypeByteConverter.BooleanToByte(booleanValue)};
-        } else if (type instanceof String[] stringArray) {
-            bytes = TypeByteConverter.stringArrayToArray(stringArray);
-        } else if (type.getClass().isEnum()) {
-            bytes = TypeByteConverter.stringToArray(type.toString());
-        } else {
-            System.err.println("Type '" + type.getClass().getSimpleName() + "'could not be converted");
-        }
+        if (byteDataType.equals(ByteDataType.String)) bytes = TypeByteConverter.stringToArray((String) value);
+        else if (byteDataType.equals(ByteDataType.Int)) bytes = TypeByteConverter.intToArray((Integer) value);
+        else if (byteDataType.equals(ByteDataType.Float)) bytes = TypeByteConverter.floatToArray((Float) value);
+        else if (byteDataType.equals(ByteDataType.Double)) bytes = TypeByteConverter.doubleToArray((Double) value);
+        else if (byteDataType.equals(ByteDataType.Short)) bytes = TypeByteConverter.shortToArray((Short) value);
+        else if (byteDataType.equals(ByteDataType.Long)) bytes = TypeByteConverter.longToArray((Long) value);
+        else if (byteDataType.equals(ByteDataType.Boolean)) bytes = new byte[]{TypeByteConverter.BooleanToByte((Boolean) value)};
+        else if (byteDataType.equals(ByteDataType.String_Array)) bytes = TypeByteConverter.stringArrayToArray((String[]) value);
+        else System.err.println("Type '" + value.getClass().getSimpleName() + "' could not be converted");
+
         return bytes;
     }
 
+    /**
+     * Returns a String representation of the ByteData object.
+     *
+     * @return a String representation of the ByteData object
+     */
     @Override
     public String toString() {
-        return "ByteData{type=" + type.getClass().getSimpleName() + ", value=" + type + '}';
+        return "ByteData{ByteDataType=" + byteDataType + ", value=" + value + '}';
     }
 }
---
+++ src/main/java/me/bredo/cmd/byteData/utilities/ByteDataType.java
@@ -0,0 +1,5 @@
+package me.bredo.cmd.byteData.utilities;
+
+public enum ByteDataType {
+    Int, String, Long, Short, Float, Double, Boolean, String_Array
+}
